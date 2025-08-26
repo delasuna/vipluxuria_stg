c8372bd189b1f6eef1dd1f8027a397451aa4211e
Cesar de Lucena <cesardelucenajr@gmail.com>
2025-08-26 20:40:46 -0300

    ajusta pagina /guia-moteis-porto-alegre-poa/

--- guia_moteis/guia_moteis_a.php
+++ guia_moteis/guia_moteis_a.php
@@ -1,22 +1,19 @@
-<? 	$conexao = require_once '../php/conecta_mysql.php';  ?>
-<?
-	$sql = " SELECT * FROM seo, tipoSeo Where seo.idTipoSeo = tipoSeo.idTipoSeo AND descricao = 'GuiasDeMotel'"; 
+<?php 
+$conexao = require_once '../php/conecta_mysql.php';  
 
-	$resultado = mysql_query($sql, $conexao);
-	if(!$resultado){
-		die("Impossível visualizar SEO: " . mysql_error() . '<br>');
-	}
+$sql = "SELECT * FROM seo, tipoSeo WHERE seo.idTipoSeo = tipoSeo.idTipoSeo AND descricao = 'GuiasDeMotel'";
+$resultado = mysqli_query($conexao, $sql);
 
-							
-	$sts = mysql_query($sql); 
-	$registros = mysql_num_rows($sts);
-	if ($registros>0) {
-		while($row = mysql_fetch_array($resultado)) {
-			$title = $row['title']; 
-			$description = $row['description'];
-			$keywords = $row['keywords'];     
-		} 
-	}
+if (!$resultado) {
+    die("Impossível visualizar SEO: " . mysqli_error($conexao) . '<br>');
+}
+
+if (mysqli_num_rows($resultado) > 0) {
+    $row = mysqli_fetch_assoc($resultado);
+    $title = $row['title'];
+    $description = $row['description'];
+    $keywords = $row['keywords'];
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -24,10 +21,10 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />  
 <meta name="robots" content="index,follow">
-<meta name="description" content="<?=$description?>" />
-<meta name="keywords" content="<?=$keywords?>" />
+<meta name="description" content="<?php echo htmlspecialchars($description); ?>" />
+<meta name="keywords" content="<?php echo htmlspecialchars($keywords); ?>" />
 
-<title>Guia de Motéis - Vip Luxúria</title>
+<title><?php echo !empty($title) ? htmlspecialchars($title) : "Guia de Motéis - Vip Luxúria"; ?></title>
 
 <!--CSS-->
 <link href="../css-js/estilos-2.css" rel="stylesheet" type="text/css" />
@@ -47,49 +44,52 @@
             <?php include("../php/topo-2.php"); ?>
         </div><!--TOPO-->
         <div id="menu">
-			<?php include("../php/menu-2.php"); ?>
+            <?php include("../php/menu-2.php"); ?>
         </div><!--MENU-->
     </div><!--BG ROSA-->
     <div id="bg-couro">    
         <div id="principal">
             <div id="principal-content-full">
-            	<div id="coluna-full">
-					<div id="titulo-pagina"><img src="../imagens/estrutura/titulo-guia-moteis.png" width="760" height="41" /></div>			
-						<a href="https://www.foumotel.com.br/" target="_blank"><img src="../imagens/banner-moteis/fou.png"></a><br><br>
-						<a href="https://www.motelmedieval.com.br/" target="_blank"><img src="../imagens/banner-moteis/medieval.png"></a><br><br>
-						<a href="https://www.motelsherwood.com.br/" target="_blank"><img src="../imagens/banner-moteis/sherwood.png"></a><br><br>						
-						<a href="https://moteisvisavis.com.br/porto-alegre" target="_blank"><img src="../imagens/banner-moteis/vis-a-vis.png"></a><br><br>
-						
-						<a href="https://www.motelcalidon.com.br/" target="_blank"><img src="../imagens/banner-moteis/calidon.png"></a><br><br>
-						<a href="https://motelcozumel.com.br/" target="_blank"><img src="../imagens/banner-moteis/cozumel.png"></a><br><br>
-						<a href="https://www.moteldosalpes.com.br/" target="_blank"><img src="../imagens/banner-moteis/alpes.png"></a><br><br>						
-						<a href="https://www.motelsahara.com.br/" target="_blank"><img src="../imagens/banner-moteis/sahara.png"></a><br><br>						
+                <div id="coluna-full">
+                    <div id="titulo-pagina"><img src="../imagens/estrutura/titulo-guia-moteis.png" width="760" height="41" /></div>            
+                    <?php
+                    // Lista de banners de motéis
+                    $banners = [
+                        ["link" => "https://www.foumotel.com.br/", "img" => "fou.png"],
+                        ["link" => "https://www.motelmedieval.com.br/", "img" => "medieval.png"],
+                        ["link" => "https://www.motelsherwood.com.br/", "img" => "sherwood.png"],
+                        ["link" => "https://moteisvisavis.com.br/porto-alegre", "img" => "vis-a-vis.png"],
+                        ["link" => "https://www.motelcalidon.com.br/", "img" => "calidon.png"],
+                        ["link" => "https://motelcozumel.com.br/", "img" => "cozumel.png"],
+                        ["link" => "https://www.moteldosalpes.com.br/", "img" => "alpes.png"],
+                        ["link" => "https://www.motelsahara.com.br/", "img" => "sahara.png"]
+                    ];
 
-						
-					<div class="clear"></div> 					
-
-								
-              </div><!-- COLUNA-FULL -->
-                                    
-				<div class="clear"></div>
+                    foreach ($banners as $banner) {
+                        echo '<a href="'.htmlspecialchars($banner['link']).'" target="_blank"><img src="../imagens/banner-moteis/'.$banner['img'].'" /></a><br><br>';
+                    }
+                    ?>
+                    <div class="clear"></div>                    
+                </div><!-- COLUNA-FULL -->
+                
+                <div class="clear"></div>
 
                 <div id="box-texto">
-					<h2>Guia de Motéis</h2>
-					<p>Nessa pagina vocês vão encontrar link e endereço para os melhores motéis de Porto Alegre e grande Porto Alegre.</p><br><br>
-					
-					<h2>O que é um motel?</h2>
-					<p>Os motéis são estabelecimentos que oferecem quartos privativos e confortáveis para momentos de intimidade. Ao contrário dos hotéis convencionais, os motéis geralmente são voltados para estadias curtas, onde casais e indivíduos podem aproveitar a privacidade e a discrição em um ambiente projetado especificamente para encontros românticos.</p>
-				</div>				
+                    <h2>Guia de Motéis</h2>
+                    <p>Nessa pagina vocês vão encontrar link e endereço para os melhores motéis de Porto Alegre e grande Porto Alegre.</p><br><br>
+                    
+                    <h2>O que é um motel?</h2>
+                    <p>Os motéis são estabelecimentos que oferecem quartos privativos e confortáveis para momentos de intimidade. Ao contrário dos hotéis convencionais, os motéis geralmente são voltados para estadias curtas, onde casais e indivíduos podem aproveitar a privacidade e a discrição em um ambiente projetado especificamente para encontros românticos.</p>
+                </div>                
 
-				
             </div><!--PRINCIPAL CONTENT FULL-->
         </div><!--PRINCIPAL-->
-	</div><!--BG-COURO-->
+    </div><!--BG-COURO-->
     <div id="rodape">
-		<?php include("../php/rodape-2.php"); ?>
+        <?php include("../php/rodape-2.php"); ?>
     </div><!--RODAPE-->
     <div id="tags">
-		<?php include("../php/tags-moteis.php"); ?>
+        <?php include("../php/tags-moteis.php"); ?>
     </div><!--TAGS-->
 </div><!--wrap-->
 <script type="text/javascript"> Cufon.now(); </script>
