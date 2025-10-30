bc52c416585e64bb6d1117ebf7714f05c6e6b508
Cesar de Lucena <cesardelucenajr@gmail.com>
2025-10-30 00:44:02 -0300

    adiciona imagens de parceiros no rodape

--- rodape-novo.php
+++ rodape-novo.php
@@ -6,7 +6,7 @@
             <div class="col-lg-3 col-md-6 mb-4">
                 <h5 class="rodape-titulo">Institucional</h5>
                 <ul class="rodape-lista">
-                   
+
                     <li><a href="/vip-luxuria.php">Sobre</a></li>
                     <li><a href="/vip-blog">Blog</a></li>
                     <li><a href="/duvidas.php">Dúvidas</a></li>
@@ -21,7 +21,7 @@
                 <ul class="rodape-lista">
                     <li><a href="javascript:carregaAnunciantes('Mulheres')">Mulheres</a></li>
                     <li><a href="javascript:carregaAnunciantes('Transex')">Transex</a></li>
-                    <li><a href="javascript:carregaAnunciantes('Casais')">Casais</a></li> 
+                    <li><a href="javascript:carregaAnunciantes('Casais')">Casais</a></li>
                     <li><a href="javascript:carregaAnunciante24Horas('Sim')">24 Horas</a></li>
                 </ul>
             </div>
@@ -42,12 +42,12 @@
                         </a>
                         <a href="https://bsky.app/profile/vipluxuria.bsky.social" target="_blank" title="Bluesky">
                             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479.815 2.736 3.713 3.66 6.383 3.364.136-.02.275-.039.415-.056-.138.022-.276.04-.415.056-3.912.58-7.387 2.005-2.83 7.078 5.013 5.19 6.87-1.113 7.823-4.308.953 3.195 2.05 9.271 7.733 4.308 4.267-4.308 1.172-6.498-2.74-7.078a8.741 8.741 0 0 1-.415-.056c.14.017.279.036.415.056 2.67.297 5.568-.628 6.383-3.364.246-.828.624-5.79.624-6.478 0-.69-.139-1.861-.902-2.206-.659-.298-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8Z"/>
+                                <path d="M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479.815 2.736 3.713 3.66 6.383 3.364.136-.02.275-.039.415-.056-.138.022-.276.04-.415.056-3.912.58-7.387 2.005-2.83 7.078 5.013 5.19 6.87-1.113 7.823-4.308.953 3.195 2.05 9.271 7.733 4.308 4.267-4.308 1.172-6.498-2.74-7.078a8.741 8.741 0 0 1-.415-.056c.14.017.279.036.415.056 2.67.297 5.568-.628 6.383-3.364.246-.828.624-5.79.624-6.478 0-.69-.139-1.861-.902-2.206-.659-.298-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8Z" />
                             </svg>
                         </a>
                     </div>
-                    
-                  
+
+
                 </div>
             </div>
 
@@ -69,20 +69,46 @@
         <!-- Seção de Parceiros com Logos -->
         <div class="parceiros-section">
             <h6 class="parceiros-titulo">Nossos Parceiros</h6>
-            <div class="parceiros-logos">
-                <a href="https://www.hotelexample.com" target="_blank" class="parceiro-logo">
-                    <img src="/imagens/parceiros/hotel-plaza.png" alt="Hotel Plaza" />
+            <div class="d-flex flex-wrap justify-content-center align-items-center gap-3 py-2 logos-parceiros">
+                <a href="http://www.vipluxuriagold.com" target="_blank">
+                    <img src="/imagens/logos/vip-luxuria-gold.png" title="Vip Luxúria Gold" alt="Vip Luxúria Gold">
                 </a>
-                <a href="https://www.motelexample.com" target="_blank" class="parceiro-logo">
-                    <img src="/imagens/parceiros/motel-vip.png" alt="Motel VIP" />
+
+                <img src="/imagens/logos/felipe-picture.png" title="Felipe Picture" alt="Felipe Picture">
+
+                <a href="https://www.acompanhantes61.com.br/" title="Acompanhantes Brasília" target="_blank">
+                    <img src="/imagens/logos/a61.png" alt="Acompanhantes de Brasília">
                 </a>
-                <a href="https://www.restaurantexample.com" target="_blank" class="parceiro-logo">
-                    <img src="/imagens/parceiros/restaurante-luxo.png" alt="Restaurante Luxo" />
+
+                <a href="https://harem69.com.br/" target="_blank">
+                    <img src="https://www.vipluxuria.com/imagens/parceiros/harem.png" title="Harem 69" alt="Harem 69">
                 </a>
-                <a href="https://www.clubexample.com" target="_blank" class="parceiro-logo">
-                    <img src="/imagens/parceiros/club-elite.png" alt="Club Elite" />
+
+                <a href="https://www.guiaadulto.com/" target="_blank">
+                    <img src="https://www.vipluxuria.com/imagens/parceiros/guia-adulto.png" title="Fórum de Acompanhantes" alt="Fórum de Acompanhantes">
+                </a>
+
+                <a href="/angelo-personal.php/">
+                    <img src="/imagens/logos/angelo-marques.png" title="Ângelo Marques Personal Trainer" alt="Ângelo Marques Personal Trainer">
+                </a>
+
+                <a href="https://www.foumotel.com.br/" target="_blank">
+                    <img src="/imagens/parceiros/fou-motel.png" alt="Fou Motel">
+                </a>
+
+                <a href="http://www.moteisvisavis.com.br/porto-alegre/" target="_blank">
+                    <img src="https://www.vipluxuria.com/imagens/parceiros/vis-a-vis.png" alt="Motel Vis-a-Vis">
+                </a>
+
+                <a href="https://www.motelmedieval.com.br/site/" target="_blank">
+                    <img src="https://www.vipluxuria.com/imagens/parceiros/medieval.png" alt="Medieval Motel Spa">
+                </a>
+
+                <a href="https://www.motelsherwood.com.br/site/" target="_blank">
+                    <img src="https://www.vipluxuria.com/imagens/parceiros/sherwood.png" alt="Sherwood Motel">
                 </a>
             </div>
+
         </div>
 
         <hr class="rodape-divisor">
@@ -91,8 +117,8 @@
         <div class="rodape-copyright">
             <p>&copy; Desde 2008 - Vip Luxúria - Todos os direitos reservados</p>
             <p class="aviso-legal">
-                Este site é destinado apenas para maiores de 18 anos. 
-  
+                Este site é destinado apenas para maiores de 18 anos.
+
             </p>
         </div>
     </div>
