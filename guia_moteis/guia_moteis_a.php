e9645599a55356ccddf839876f92b3824788e8d7
Cesar de Lucena <cesardelucenajr@gmail.com>
2025-12-19 21:13:24 -0300

    ajusta pagina guia de moteis

--- guia_moteis/guia_moteis_a.php
+++ guia_moteis/guia_moteis_a.php
@@ -1,11 +1,13 @@
 <?php 
 $conexao = require_once '../php/conecta_mysql.php';  
 
-$sql = "SELECT * FROM seo, tipoSeo WHERE seo.idTipoSeo = tipoSeo.idTipoSeo AND descricao = 'GuiasDeMotel'";
+$sql = "SELECT * FROM seo, tipoSeo 
+        WHERE seo.idTipoSeo = tipoSeo.idTipoSeo 
+        AND descricao = 'GuiasDeMotel'";
 $resultado = mysqli_query($conexao, $sql);
 
 if (!$resultado) {
-    die("Impossível visualizar SEO: " . mysqli_error($conexao) . '<br>');
+    die("Impossível visualizar SEO: " . mysqli_error($conexao));
 }
 
 if (mysqli_num_rows($resultado) > 0) {
@@ -16,67 +18,84 @@ if (mysqli_num_rows($resultado) > 0) {
 }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-BR" xml:lang="pt-BR"> 
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-<?php include 'head.php'; ?>
+<?php include '../head.php'; ?>
 
 <body>
 <div id="wrap">
-    <div id="bg-rosa">
-        <div id="topo">
-            <?php include("../php/topo-2.php"); ?>
-        </div><!--TOPO-->
-        <div id="menu">
-            <?php include("../php/menu-2.php"); ?>
-        </div><!--MENU-->
-    </div><!--BG ROSA-->
-    <div id="bg-couro">    
-        <div id="principal">
-            <div id="principal-content-full">
-                <div id="coluna-full">
-                    <div id="titulo-pagina"><img src="../imagens/estrutura/titulo-guia-moteis.png" width="760" height="41" /></div>            
+
+    <div>
+        <?php include("../php/menu-2.php"); ?>
+    </div>
+
+    <div class="degrade">
+        <div class="moteis-page-premium">
+            <div class="container">
+
+                <!-- Header -->
+                <div class="dicas-header-premium pt-5">
+                    <h1 class="dicas-title-main">
+                        <i class="bi bi-house-heart"></i>
+                        Guia de <span class="text-rosa">Motéis</span>
+                    </h1>
+                </div>
+
+                <!-- Lista de Motéis (coluna única) -->
+                <div class="dicas-grid-premium" style="margin-bottom: 0 !important;">
+
                     <?php
-                    // Lista de banners de motéis
                     $banners = [
-                        ["link" => "https://www.foumotel.com.br/", "img" => "fou.png"],
-                        ["link" => "https://www.motelmedieval.com.br/", "img" => "medieval.png"],
-                        ["link" => "https://www.motelsherwood.com.br/", "img" => "sherwood.png"],
-                        ["link" => "https://moteisvisavis.com.br/porto-alegre", "img" => "vis-a-vis.png"],
-                        ["link" => "https://www.motelcalidon.com.br/", "img" => "calidon.png"],
-                        ["link" => "https://motelcozumel.com.br/", "img" => "cozumel.png"],
-                        ["link" => "https://www.moteldosalpes.com.br/", "img" => "alpes.png"],
-                        ["link" => "https://www.motelsahara.com.br/", "img" => "sahara.png"]
+                        ["link" => "https://www.foumotel.com.br/", "img" => "fou.png", "nome" => "Fou Motel"],
+                        ["link" => "https://www.motelmedieval.com.br/", "img" => "medieval.png", "nome" => "Motel Medieval"],
+                        ["link" => "https://www.motelsherwood.com.br/", "img" => "sherwood.png", "nome" => "Motel Sherwood"],
+                        ["link" => "https://moteisvisavis.com.br/porto-alegre", "img" => "vis-a-vis.png", "nome" => "Motel Vis a Vis"],
+                        ["link" => "https://www.motelcalidon.com.br/", "img" => "calidon.png", "nome" => "Motel Calidon"],
+                        ["link" => "https://motelcozumel.com.br/", "img" => "cozumel.png", "nome" => "Motel Cozumel"],
+                        ["link" => "https://www.moteldosalpes.com.br/", "img" => "alpes.png", "nome" => "Motel dos Alpes"],
+                        ["link" => "https://www.motelsahara.com.br/", "img" => "sahara.png", "nome" => "Motel Sahara"]
                     ];
 
-                    foreach ($banners as $banner) {
-                        echo '<a href="'.htmlspecialchars($banner['link']).'" target="_blank"><img src="../imagens/banner-moteis/'.$banner['img'].'" /></a><br><br>';
-                    }
-                    ?>
-                    <div class="clear"></div>                    
-                </div><!-- COLUNA-FULL -->
-                
-                <div class="clear"></div>
+                    $i = 1;
+                    foreach ($banners as $banner): ?>
+                        <div class="dica-card">
+                            <div class="dica-number">
+                                <?php echo str_pad($i++, 2, '0', STR_PAD_LEFT); ?>
+                            </div>
+                            <div class="dica-content text-center">
+                                <a href="<?php echo htmlspecialchars($banner['link']); ?>" 
+                                   target="_blank" rel="noopener">
+                                    <img 
+                                        src="../imagens/banner-moteis/<?php echo htmlspecialchars($banner['img']); ?>" 
+                                        alt="<?php echo htmlspecialchars($banner['nome']); ?>" 
+                                        class="img-fluid rounded mb-3"
+                                    >
+                                </a>
+                                <h3><?php echo htmlspecialchars($banner['nome']); ?></h3>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
 
-                <div id="box-texto">
-                    <h2>Guia de Motéis</h2>
-                    <p>Nessa pagina vocês vão encontrar link e endereço para os melhores motéis de Porto Alegre e grande Porto Alegre.</p><br><br>
-                    
-                    <h2>O que é um motel?</h2>
-                    <p>Os motéis são estabelecimentos que oferecem quartos privativos e confortáveis para momentos de intimidade. Ao contrário dos hotéis convencionais, os motéis geralmente são voltados para estadias curtas, onde casais e indivíduos podem aproveitar a privacidade e a discrição em um ambiente projetado especificamente para encontros românticos.</p>
-                </div>                
+                    <!-- Texto explicativo -->
+                    <div class="dicas-intro-card">
+                        <h2>O que é um motel?</h2>
+                        <p>
+                            Os motéis são estabelecimentos que oferecem quartos privativos e confortáveis para momentos de intimidade. Ao contrário dos hotéis convencionais, os motéis geralmente são voltados para estadias curtas, onde casais e indivíduos podem aproveitar a privacidade e a discrição em um ambiente projetado especificamente para encontros românticos.
+                        </p>
+                    </div>
+                </div>
 
-            </div><!--PRINCIPAL CONTENT FULL-->
-        </div><!--PRINCIPAL-->
-    </div><!--BG-COURO-->
-    <div id="rodape">
-        <?php include("../php/rodape-2.php"); ?>
-    </div><!--RODAPE-->
-    <div id="tags">
-        <?php include("../php/tags-moteis.php"); ?>
-    </div><!--TAGS-->
-</div><!--wrap-->
-<script type="text/javascript"> Cufon.now(); </script>
+
+            </div>
+        </div>
+    </div>
+
+    <?php include("../rodape-novo.php"); ?>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 <?php include("../php/google.php"); ?>
 
 </body>
